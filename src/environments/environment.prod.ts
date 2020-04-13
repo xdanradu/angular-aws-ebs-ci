000854e0937e4ae8a0ca77178cd7b1caacfb6d06
Dan Radu <xdanradu@yahoo.com>
2020-04-13 20:42:54 +0300

    X

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api: 'http://expressangular-env.eba-ejbf5vnr.us-east-1.elasticbeanstalk.com', 
+  apiUrl: 'http://expressangular-env.eba-ejbf5vnr.us-east-1.elasticbeanstalk.com', 
   production: true
 };
